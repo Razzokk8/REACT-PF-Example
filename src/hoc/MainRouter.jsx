7ca853865d6af84ec29bf2f4dbb05c6565768467
Riccardo Limiti <razzokk@f514b3c2-138c-4ebf-b107-501ba7cd66d3.fritz.box>
2022-11-28 21:15:58 +0100

    testing

--- src/hoc/MainRouter.jsx
+++ src/hoc/MainRouter.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import About from '../components/About'
-import Design from '../components/Design'
-import Foot from '../components/Footer'
-import GetInTouch from '../components/GetInTouch'
-import Home from '../components/Home'
-import Music from '../components/Music'
-import Tech from '../components/Technologies'
-import WebProjects from '../components/WebProjects'
-
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import About from "../components/About";
+import Design from "../components/Design";
+import Foot from "../components/Footer";
+import GetInTouch from "../components/GetInTouch";
+import Home from "../components/Home";
+import Music from "../components/Music";
+import Tech from "../components/Technologies";
+import WebProjects from "../components/WebProjects";
 
 function MainR() {
-    return (
-        <div>
-            <Home />
-           <About />
-            <Tech />
-             <WebProjects />
-              <Design />
-             <Music /> 
-             <GetInTouch />
-            <Foot />
-        </div>
-    )
+  return (
+    <div>
+      <Home />
+      <About />
+      <Tech />
+      <WebProjects />
+      <Design />
+      <Music />
+      <GetInTouch />
+      <Foot />
+    </div>
+  );
 }
 
-export default MainR
+export default MainR;
