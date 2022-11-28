f1728d2e013a59e721208e52405503fb8ab2e193
Riccardo Limiti <razzokk@f514b3c2-138c-4ebf-b107-501ba7cd66d3.fritz.box>
2022-11-28 20:17:10 +0100

    testing

--- src/hoc/MainRouter.jsx
+++ src/hoc/MainRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import About from '../components/About'
-// import Design from '../components/Design'
+import Design from '../components/Design'
 import Foot from '../components/Footer'
 import GetInTouch from '../components/GetInTouch'
 import Home from '../components/Home'
