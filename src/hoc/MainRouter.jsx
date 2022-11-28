6d8f232434b5166516dadc1f5527e6f325b18e60
Riccardo Limiti <razzokk@f514b3c2-138c-4ebf-b107-501ba7cd66d3.fritz.box>
2022-11-28 19:43:54 +0100

    testing

--- src/hoc/MainRouter.jsx
+++ src/hoc/MainRouter.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-// import About from '../components/About'
+import About from '../components/About'
 // import Design from '../components/Design'
 // import Foot from '../components/Footer'
 // import GetInTouch from '../components/GetInTouch'
 import Home from '../components/Home'
 // import Music from '../components/Music'
-// import Tech from '../components/Technologies'
-// import WebProjects from '../components/WebProjects'
+import Tech from '../components/Technologies'
+import WebProjects from '../components/WebProjects'
 
 
 function MainR() {
     return (
         <div>
             <Home />
-            {/* <About />
+           <About />
             <Tech />
              <WebProjects />
-             <Design />
+              {/* <Design />
              <Music /> 
              <GetInTouch />
             <Foot />  */}
