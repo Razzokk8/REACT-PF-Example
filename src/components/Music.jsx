a240b408b6bed111349f24b68bb8f9565fd14334
Riccardo Limiti <razzokk@MacBook-Pro-di-Riccardo.fritz.box>
2022-12-01 16:15:32 +0100

    testing

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import BannerMusic from "./ParallaxMusic";
+import BannerMusic from "./ParallaxMusic";
 
 function Music() {
   return (
     <div id="music">
-      {/* <BannerMusic /> */}
+      <BannerMusic /> 
     </div>
   );
 }
