ac6097acaea1c2fa9f796321a4e9c704d44b0a3e
Riccardo Limiti <razzokk@MacBook-Pro-di-Riccardo.fritz.box>
2022-12-01 15:40:41 +0100

    testing

--- src/hoc/MainRouter.jsx
+++ src/hoc/MainRouter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import About from "../components/About";
-// import Design from "../components/Design";
+import Design from "../components/Design";
 import Foot from "../components/Footer";
 import GetInTouch from "../components/GetInTouch";
 import Home from "../components/Home";
@@ -15,9 +15,9 @@ function MainR() {
       <Home />
       <About />
       <Tech />
-       <WebProjects />
-      {/* <Design />  */}
-       <Music /> 
+      <WebProjects />
+      <Design />
+      <Music />
       <GetInTouch />
       <Foot />
     </div>
